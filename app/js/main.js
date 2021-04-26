cf8eb03942111bb1ce5da958a7efcf648dce20d8
Danya <d.kashapov@napoleonit.ru>
2021-04-27 02:16:58 +0300

    header add new animation and user-panel

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,26 @@
 $(function () {
 
+    headerSearchToggle = function () {
+        
+        let headerSearchOpenBtn = document.querySelector('.header__search-open-btn');
+        let headerSearchForm = document.querySelector('.header__search-form');
+
+        return headerSearchOpenBtn.addEventListener('click', function (event) {
+            event.preventDefault;
+
+            if (headerSearchForm.classList.contains('header__search-form-disable')) {
+                headerSearchOpenBtn.classList.add('header__search-open-btn--active');
+                headerSearchForm.classList.remove('header__search-form-disable');
+
+            }
+            else {
+                headerSearchOpenBtn.classList.remove('header__search-open-btn--active');
+                headerSearchForm.classList.add('header__search-form-disable');
+            }
+        });
+    }();
+    
+        
+
+    
 })
